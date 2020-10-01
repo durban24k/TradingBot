3dc78f1493f5809c0cafdef5005ffe544422057d
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-10-01 19:15:53 +0300

    v1.1

--- robot.py
+++ robot.py
@@ -1,10 +1,19 @@
 import MetaTrader5 as mt5
 
-if not mt5.initialize(login=35564619,server="MetaQuotes-Demo",password="pDn3HkjZ"):
+if not mt5.initialize():
      print("initialize() failed, error code =",mt5.last_error())
      quit()
 
-print(mt5.terminal_info())
-print(mt5.version())
+account=35564619
+password="pDn3HkjZ"
+authorize_login = mt5.login(account,password)
+if authorize_login:
+     print(mt5.account_info())
+     print("Show account_info()")
+     account_info_dict=mt5.account_info()._asdict()
+     for prop in account_info_dict:
+          print("   {}={}".format(prop,account_info_dict[prop]))
+else:
+     print("failed to connect at account #{}, error code: {}".format(account, mt5.last_error()))
 
 mt5.shutdown()
